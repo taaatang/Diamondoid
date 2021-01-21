15c6d993e39348dd6db494c084e686479a8f2550
taaatang <taaatang@outlook.com>
2021-01-21 10:19:40 -0800

    random number generator

--- src_config/random.hpp
+++ src_config/random.hpp
@@ -1,5 +1,17 @@
 #ifndef __RANDOM_H__
 #define __RANDOM_H__
 
+#include <random>
+#include <functional>
+std::mt19937 generatorI;
+template<size_t N>
+std::uniform_int_distribution<int> distributionI(0,N);
+template<size_t N>
+int diceI(){return distributionI<N>(generatorI);}
+
+std::mt19937 generatorD;
+std::uniform_real_distribution<double> distributionD(0.0,1.0);
+auto diceD = std::bind(distributionD,generatorD);
+
 
 #endif // __RANDOM_H__
